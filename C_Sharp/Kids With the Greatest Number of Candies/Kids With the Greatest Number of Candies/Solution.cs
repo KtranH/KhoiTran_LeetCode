503e61520af205048810a8bf97c7076b90403c23
KtranH <87740741+KtranH@users.noreply.github.com>
2025-01-29 21:32:25 +0700

    sloved Kids With the Greatest Number of Candies

---
+++ C_Sharp/Kids With the Greatest Number of Candies/Kids With the Greatest Number of Candies/Solution.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kids_With_the_Greatest_Number_of_Candies
+{
+    public class Solution
+    {
+        public bool[] sloved_kids_the_greatest_number_of_candies(int[] candies, int extraCandies)
+        {
+            int max = candies.Max();
+            bool[] result = new bool[candies.Length];
+            for (int i = 0; i < candies.Length; i++)
+            {
+                if (candies[i] + extraCandies >= max)
+                {
+                    result[i] = true;
+                }
+                else
+                {
+                    result[i] = false;
+                }    
+            }
+            return result;
+        }
+    }
+}
