162de156b5439debeebbbd8ef6edfe00ab2c64df
KtranH <87740741+KtranH@users.noreply.github.com>
2025-01-31 22:29:46 +0700

    Sloved Reverse Vowels of a String

---
+++ C_Sharp/Reverse Vowels of a String/Reverse Vowels of a String/Program.cs
@@ -0,0 +1,36 @@
+﻿//Example 1:
+
+//Input: s = "IceCreAm"
+
+//Output: "AceCreIm"
+
+//Explanation:
+
+//The vowels in s are['I', 'e', 'e', 'A']. On reversing the vowels, s becomes "AceCreIm".
+
+//Example 2:
+
+//Input: s = "leetcode"
+
+//Output: "leotcede
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse_Vowels_of_a_String
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] vowels = { "a", "e", "i", "o", "u", "A", "E", "I", "O", "U" };
+            string s = "leetcode";
+            Solution solution = new Solution();
+            Console.WriteLine(solution.slove_reverse_vowels(s, vowels));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C_Sharp/Reverse Vowels of a String/Reverse Vowels of a String/Solution.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse_Vowels_of_a_String
+{
+    public class Solution
+    {
+        public string slove_reverse_vowels(string s, string[] vowels)
+        {
+            int count = s.Length;
+            StringBuilder sb = new StringBuilder(s);
+            for(int i = 0; i < sb.Length; i++)
+            {
+                if(vowels.Contains(sb[i].ToString()))
+                {
+                    for(int j = (sb.Length - 1) - (sb.Length - count); j > i; j--)
+                    {
+                        if(vowels.Contains(sb[j].ToString()))
+                        {
+                            char temp = sb[i];
+                            sb[i] = sb[j];
+                            sb[j] = temp;
+                            count = j;
+                            break;
+                        }
+                    }    
+                }    
+            }
+            s = sb.ToString();
+            return s;
+        }
+    }
+}
