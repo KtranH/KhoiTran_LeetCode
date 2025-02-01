d3ec6db5d112b352bc19f627ea524240e1d57146
KtranH <87740741+KtranH@users.noreply.github.com>
2025-02-01 20:40:40 +0700

    Sloved Reverse Words in a String

---
+++ C_Sharp/Reverse Words in a String/Reverse Words in a String/Program.cs
@@ -0,0 +1,33 @@
+﻿//Example 1:
+
+//Input: s = "the sky is blue"
+//Output: "blue is sky the"
+//Example 2:
+
+//Input: s = "  hello world  "
+//Output: "world hello"
+//Explanation: Your reversed string should not contain leading or trailing spaces.
+//Example 3:
+
+//Input: s = "a good   example"
+//Output: "example good a"
+//Explanation: You need to reduce multiple spaces between two words to a single space in the reversed string.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse_Words_in_a_String
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            Console.WriteLine(solution.slove_reverse_string("the sky is blue"));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C_Sharp/Reverse Words in a String/Reverse Words in a String/Solution.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Reverse_Words_in_a_String
+{
+    public class Solution
+    {
+        public string slove_reverse_string(string s)
+        {
+            //string result = string.Join(" ", s.Split(' ').Reverse()).Trim();
+            //return Regex.Replace(result, @"\s+", " ");
+            string result = s.Trim();
+            for (int i = 0; i < result.Length; i++)
+            {
+                if(result[i] == ' ' && result[i + 1] == ' ')
+                {
+                    result = result.Remove(i, 1);
+                    i --;
+                }    
+            }    
+            return string.Join(" ", result.Split(' ').Reverse());
+        }
+    }
+}
