54a6e1f19867d13efac377a337e3c7af8476261d
KtranH <87740741+KtranH@users.noreply.github.com>
2025-02-01 20:45:41 +0700

    Sloved Reverse Words in a String

---
+++ JavaScript/Reverse Words in a String/app.js
@@ -0,0 +1,22 @@
+/*Example 1:
+
+Input: s = "the sky is blue"
+Output: "blue is sky the"
+Example 2:
+
+Input: s = "  hello world  "
+Output: "world hello"
+Explanation: Your reversed string should not contain leading or trailing spaces.
+Example 3:
+
+Input: s = "a good   example"
+Output: "example good a"
+Explanation: You need to reduce multiple spaces between two words to a single space in the reversed string.*/
+
+const s = "the sky is blue";
+
+function slove_reverse_words(s) {
+    return s.split(' ').filter(word => word !== '').reverse().join(' ');
+}
+
+console.log(slove_reverse_words(s));
