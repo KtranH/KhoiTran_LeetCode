a6114b0460e6c91ef1330824022a7491251238ad
KtranH <87740741+KtranH@users.noreply.github.com>
2025-02-02 23:12:05 +0700

    Sloved Product of Array Except Self

---
+++ C_Sharp/Product of Array Except Self/Product of Array Except Self/Program.cs
@@ -0,0 +1,29 @@
+﻿//Example 1:
+
+//Input: nums = [1, 2, 3, 4]
+//Output: [24,12,8,6]
+//Example 2:
+
+//Input: nums = [-1, 1, 0, -3, 3]
+//Output: [0,0,9,0,0]
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Product_of_Array_Except_Self
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ints = { 1, 2, 3, 4 };
+            Solution solution = new Solution();
+            int[] result = solution.slove_product_of_array_except_self(ints);
+            Console.WriteLine(string.Join(",", result));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C_Sharp/Product of Array Except Self/Product of Array Except Self/Solution.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Product_of_Array_Except_Self
+{
+    public class Solution
+    {
+        public int[] slove_product_of_array_except_self(int[] nums)
+        {
+            int[] result = new int[nums.Length];
+            int pre_i = 1;
+            for(int i = 0; i < nums.Length; i++)
+            {
+                result[i] = pre_i;
+                pre_i *= nums[i];
+            }
+            int after_i = 1;
+            for(int i = nums.Length - 1; i >= 0; i--)
+            {
+                result[i] *= after_i;
+                after_i *= nums[i];
+            }
+            return result;
+        }
+    }
+}
