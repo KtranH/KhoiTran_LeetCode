4923f5b78722e41a2d8ee64ec751c0551b5394ac
KtranH <87740741+KtranH@users.noreply.github.com>
2025-02-02 23:15:54 +0700

    Sloved Product of Array Except Self

---
+++ JavaScript/Product of Array Except Self/app.js
@@ -0,0 +1,27 @@
+/*Example 1:
+
+Input: nums = [1,2,3,4]
+Output: [24,12,8,6]
+Example 2:
+
+Input: nums = [-1,1,0,-3,3]
+Output: [0,0,9,0,0]*/
+
+const nums = [1, 2, 3, 4];
+
+function slove_product_of_array_except_self(nums) {
+    let result = [];
+    let prefix = 1;
+    for (let i = 0; i < nums.length; i++) {
+        result[i] = prefix;
+        prefix *= nums[i];
+    }
+    let postfix = 1;
+    for (let i = nums.length - 1; i >= 0; i--) {
+        result[i] *= postfix;
+        postfix *= nums[i];
+    }
+    return result;
+}
+
+console.log(slove_product_of_array_except_self(nums));
