ce1f71b64ea535f95e1e3e4fa7991c311c1d544e
KtranH <87740741+KtranH@users.noreply.github.com>
2025-02-03 22:02:16 +0700

    Sloved Increasing Triplet Subsequence

---
+++ C_Sharp/Increasing Triplet Subsequence/Increasing Triplet Subsequence/Program.cs
@@ -0,0 +1,35 @@
+﻿//Example 1:
+
+//Input: nums = [1, 2, 3, 4, 5]
+//Output: true
+//Explanation: Any triplet where i<j < k is valid.
+//Example 2:
+
+//Input: nums = [5, 4, 3, 2, 1]
+//Output: false
+//Explanation: No triplet exists.
+//Example 3:
+
+//Input: nums = [2, 1, 5, 0, 4, 6]
+//Output: true
+//Explanation: The triplet(3, 4, 5) is valid because nums[3] == 0 < nums[4] == 4 < nums[5] == 6.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Increasing_Triplet_Subsequence
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 2, 3, 4, 5 };
+            Solution solution = new Solution();
+            Console.WriteLine(solution.slove_increasing_triplet_subsequence(nums));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C_Sharp/Increasing Triplet Subsequence/Increasing Triplet Subsequence/Solution.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Increasing_Triplet_Subsequence
+{
+    public class Solution
+    {
+        public bool slove_increasing_triplet_subsequence(int[] nums)
+        {
+            int max_muns_first = nums.Max() + 1;
+            int max_muns_second = nums.Max() + 1;
+            foreach (int num in nums)
+            {
+                if (num < max_muns_first)
+                {
+                    max_muns_first = num;
+                }
+                else if (num < max_muns_second)
+                {
+                    max_muns_second = num;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
