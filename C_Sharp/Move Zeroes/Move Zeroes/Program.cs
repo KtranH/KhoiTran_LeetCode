a961e959085cec09b8fbe3e40adb03d3e1714fbf
KtranH <87740741+KtranH@users.noreply.github.com>
2025-02-05 21:54:07 +0700

    Solved Move Zeroes

---
+++ C_Sharp/Move Zeroes/Move Zeroes/Program.cs
@@ -0,0 +1,28 @@
+﻿//Example 1:
+
+//Input: nums = [0, 1, 0, 3, 12]
+//Output: [1,3,12,0,0]
+//Example 2:
+
+//Input: nums = [0]
+//Output: [0]
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Move_Zeroes
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = new List<int> { 0, 1, 2, 0, 3, 12 };
+            Solution solution = new Solution();
+            solution.solve_move_zeroes(nums);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C_Sharp/Move Zeroes/Move Zeroes/Solution.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Move_Zeroes
+{
+    public class Solution
+    {
+        public void solve_move_zeroes(List<int> nums)
+        {
+            int not_zero_index = 0;
+            for (int i = 0; i < nums.Count; i++)
+            {
+                if (nums[i] != 0)
+                {
+                    nums[not_zero_index] = nums[i];
+                    not_zero_index++;
+                }
+            }
+            for(int i = not_zero_index; i < nums.Count; i++)
+            {
+                nums[i] = 0;
+            }
+        }
+    }
+}
