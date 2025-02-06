fb7cb611dcd085639c77a5464a95ef4984e9d748
KtranH <87740741+KtranH@users.noreply.github.com>
2025-02-06 23:17:13 +0700

    Solved Is Subsequence

---
+++ C_Sharp/Is Subsequence/Is Subsequence/Program.cs
@@ -0,0 +1,30 @@
+﻿//Example 1:
+
+//Input: s = "abc", t = "ahbgdc"
+//Output: true
+//Example 2:
+
+//Input: s = "axc", t = "ahbgdc"
+//Output: false
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Is_Subsequence
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "abc";
+            string t = "ahbgdc";
+
+            Solution solution = new Solution();
+            Console.WriteLine(solution.solve_is_subsequence(s, t));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C_Sharp/Is Subsequence/Is Subsequence/Solution.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Is_Subsequence
+{
+    public class Solution
+    {
+        public bool solve_is_subsequence(string s, string t)
+        {
+            if (s.Length == 0) return true;
+            int checkindex = 0;
+            for (int i = 0; i < t.Length; i++)
+            {
+                if (s[checkindex] == t[i])
+                {
+                    checkindex++;
+                    if (checkindex == s.Length)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
