1b92422debddd531c2928819ddef47be36f9ffac
KtranH <87740741+KtranH@users.noreply.github.com>
2025-02-07 21:36:13 +0700

    Solved Container With Most Water

---
+++ C_Sharp/Container With Most Water/Container With Most Water/Program.cs
@@ -0,0 +1,28 @@
+﻿//Example 1:
+//Input: height = [1, 8, 6, 2, 5, 4, 8, 3, 7]
+//Output: 49
+//Explanation: The above vertical lines are represented by array [1, 8, 6, 2, 5, 4, 8, 3, 7].In this case, the max area of water(blue section) the container can contain is 49.
+//Example 2:
+
+//Input: height = [1, 1]
+//Output: 1
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Container_With_Most_Water
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] height = { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
+            Solution s = new Solution();
+            Console.WriteLine(s.solve_container_with_most_water(height));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C_Sharp/Container With Most Water/Container With Most Water/Solution.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Container_With_Most_Water
+{
+    public class Solution
+    {
+        public int solve_container_with_most_water(int[] height) {
+            int left_index = 0;
+            int right_index = height.Length - 1;
+            int max_water = 0;
+            while (left_index < right_index) {
+                int temp = Math.Min(height[left_index], height[right_index]) * (right_index - left_index);
+                max_water = Math.Max(max_water, temp);
+                if (height[left_index] < height[right_index]) {
+                    left_index++;
+                } else {
+                    right_index--;
+                }
+            }
+            return max_water;
+        }
+    }
+}
