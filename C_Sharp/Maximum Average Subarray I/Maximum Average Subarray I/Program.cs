759799c144c09db24cd9d86d5d937c7018c8ed28
KtranH <87740741+KtranH@users.noreply.github.com>
2025-02-09 21:24:00 +0700

    Solved Maximum Average Subarray I

---
+++ C_Sharp/Maximum Average Subarray I/Maximum Average Subarray I/Program.cs
@@ -0,0 +1,29 @@
+﻿//Example 1:
+
+//Input: nums = [1, 12, -5, -6, 50, 3], k = 4
+//Output: 12.75000
+//Explanation: Maximum average is (12 - 5 - 6 + 50) / 4 = 51 / 4 = 12.75
+//Example 2:
+
+//Input: nums = [5], k = 1
+//Output: 5.00000
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maximum_Average_Subarray_I
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 12, -5, -6, 50, 3 };
+            Solution solution = new Solution();
+            Console.WriteLine(solution.solve_maxium_average_subarray(nums, 4));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C_Sharp/Maximum Average Subarray I/Maximum Average Subarray I/Solution.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maximum_Average_Subarray_I
+{
+    public class Solution
+    {
+        public double solve_maxium_average_subarray(int[] nums, int k)
+        {
+            double max_average = 0;
+            //double max_average = nums.Take(k).Sum();
+            for (int i = 0; i < k; i++)
+            {
+                max_average += nums[i];
+            }    
+            double current_average = max_average;
+            for (int i = k;  i < nums.Length; i++)
+            {
+                current_average += nums[i] - nums[i - k];
+                max_average = Math.Max(max_average, current_average);
+            }    
+            return Math.Round(max_average / k, 5);
+        }
+    }
+}
