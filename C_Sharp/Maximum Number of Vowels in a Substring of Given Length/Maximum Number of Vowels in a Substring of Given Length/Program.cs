48629013c1828f2abe5e2136fcf611b295375e40
KtranH <87740741+KtranH@users.noreply.github.com>
2025-02-10 19:26:43 +0700

    Solved Maximum Number of Vowels in a Substring of Given Length

---
+++ C_Sharp/Maximum Number of Vowels in a Substring of Given Length/Maximum Number of Vowels in a Substring of Given Length/Program.cs
@@ -0,0 +1,36 @@
+﻿//Example 1:
+
+//Input: s = "abciiidef", k = 3
+//Output: 3
+//Explanation: The substring "iii" contains 3 vowel letters.
+//Example 2:
+
+//Input: s = "aeiou", k = 2
+//Output: 2
+//Explanation: Any substring of length 2 contains 2 vowels.
+//Example 3:
+
+//Input: s = "leetcode", k = 3
+//Output: 2
+//Explanation: "lee", "eet" and "ode" contain 2 vowels.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maximum_Number_of_Vowels_in_a_Substring_of_Given_Length
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "abciiidef";
+            int k = 3;
+            Solution solution = new Solution();
+            Console.WriteLine(solution.solve_maximum_number_of_vowels_in_a_substring_of_given_length(s, k));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C_Sharp/Maximum Number of Vowels in a Substring of Given Length/Maximum Number of Vowels in a Substring of Given Length/Solution.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maximum_Number_of_Vowels_in_a_Substring_of_Given_Length
+{
+    public class Solution
+    {
+        public int solve_maximum_number_of_vowels_in_a_substring_of_given_length(string s, int k)
+        {
+            char[] vowels = {'a', 'e', 'i', 'o', 'u'};
+            int sliding_window = 0;
+            for (int i = 0; i < k; i++)
+            {
+                if(vowels.Contains(s[i]))
+                {
+                    sliding_window++;
+                }    
+            }
+            int max_vowels = sliding_window;
+            for (int i = k; i < s.Length; i++)
+            {
+                if(vowels.Contains(s[i - k]))
+                {
+                    sliding_window--;
+                }    
+                if(vowels.Contains(s[i]))
+                {
+                    sliding_window++;
+                }    
+                if(sliding_window > max_vowels)
+                {
+                    max_vowels = sliding_window;
+                }    
+            }    
+            return max_vowels;
+        }
+    }
+}
