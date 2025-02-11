e05fac45a92e248ab6505513607561282421d8ef
KtranH <87740741+KtranH@users.noreply.github.com>
2025-02-11 19:52:54 +0700

    Solved Max Consecutive Ones III

---
+++ C_Sharp/Max Consecutive Ones III/Max Consecutive Ones III/Program.cs
@@ -0,0 +1,34 @@
+﻿//Example 1:
+
+//Input: nums = [1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0], k = 2
+//Output: 6
+//Explanation: [1,1,1,0,0,1,1,1,1,1,1]
+//Bolded numbers were flipped from 0 to 1. The longest subarray is underlined.
+//Example 2:
+
+//Input: nums = [0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1], k = 3
+//Output: 10
+//Explanation: [0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,1,1,1,1]
+//Bolded numbers were flipped from 0 to 1. The longest subarray is underlined.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Max_Consecutive_Ones_III
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[]{ 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0 };
+            int k = 3;
+
+            Solution solution = new Solution();
+            Console.WriteLine(solution.solve_max_consecutive_ones_iii(nums, k));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C_Sharp/Max Consecutive Ones III/Max Consecutive Ones III/Solution.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Max_Consecutive_Ones_III
+{
+    public class Solution
+    {
+        public int solve_max_consecutive_ones_iii(int[] nums, int k)
+        {
+            int left_index = 0;
+            int zero_count = 0;
+            int max_length = 0;
+
+            for(int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == 0) {
+                    zero_count++;
+                }
+                if (zero_count > k){
+                    if (nums[left_index] == 0)
+                    {
+                        zero_count--;
+                    }
+                    left_index++;
+                }
+                int sliding_window = i - left_index + 1;
+                if(max_length < sliding_window) max_length = sliding_window;
+            }
+            return max_length;
+        }
+    }
+}
