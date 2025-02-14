bc70f34d606243e3c9c330592e1ebb79241fc7d2
KtranH <87740741+KtranH@users.noreply.github.com>
2025-02-14 19:40:57 +0700

    Solved Find Pivot Index

---
+++ C_Sharp/Find Pivot Index/Find Pivot Index/Solution.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Find_Pivot_Index
+{
+    public class Solution
+    {
+        public int solve_find_pivot_index(int[] nums)
+        {
+            int total_sum = 0;
+            for(int i = 0; i < nums.Length; i++)
+            {
+                total_sum += nums[i];
+            }
+
+            int left_sum = 0;
+            for(int i = 0; i < nums.Length; i++)
+            {
+                if(left_sum == total_sum - left_sum - nums[i])
+                {
+                    return i;
+                }
+                left_sum += nums[i];
+            }
+            return -1;
+        }
+    }
+}
