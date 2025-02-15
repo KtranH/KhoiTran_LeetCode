27c487fc527c639cec7cb5c4527780a0c107430b
KtranH <87740741+KtranH@users.noreply.github.com>
2025-02-15 18:29:47 +0700

    Solved Find the Difference of Two Arrays

---
+++ C_Sharp/Find the Difference of Two Arrays/Find the Difference of Two Arrays/Program.cs
@@ -0,0 +1,47 @@
+﻿//Example 1:
+
+//Input: nums1 = [1, 2, 3], nums2 = [2, 4, 6]
+//Output: [[1,3],[4,6]]
+//Explanation:
+//For nums1, nums1[1] = 2 is present at index 0 of nums2, whereas nums1[0] = 1 and nums1[2] = 3 are not present in nums2. Therefore, answer[0] = [1, 3].
+//For nums2, nums2[0] = 2 is present at index 1 of nums1, whereas nums2[1] = 4 and nums2[2] = 6 are not present in nums2. Therefore, answer[1] = [4, 6].
+//Example 2:
+
+//Input: nums1 = [1, 2, 3, 3], nums2 = [1, 1, 2, 2]
+//Output: [[3],[]]
+//Explanation:
+//For nums1, nums1[2] and nums1[3] are not present in nums2. Since nums1[2] == nums1[3], their value is only included once and answer[0] = [3].
+//Every integer in nums2 is present in nums1. Therefore, answer[1] = [].
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Find_the_Difference_of_Two_Arrays
+{
+    public class Program
+    {
+        static void print_list(IList<IList<int>> result)
+        {
+            foreach (IList<int> list in result)
+            {
+                foreach (int num in list)
+                {
+                    Console.Write(num + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[] nums1 = new int[] { 1, 2, 3 };
+            int[] nums2 = new int[] { 2, 4, 6 };
+            Solution s = new Solution();
+            IList<IList<int>> result = s.solve_find_the_difference_of_two_arrays(nums1, nums2);
+            print_list(result);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C_Sharp/Find the Difference of Two Arrays/Find the Difference of Two Arrays/Solution.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Find_the_Difference_of_Two_Arrays
+{
+    public class Solution
+    {
+        public IList<IList<int>> solve_find_the_difference_of_two_arrays(int[] nums1, int[] nums2) {
+            HashSet<int> set1 = new HashSet<int>(nums1);
+            HashSet<int> set2 = new HashSet<int>(nums2);
+
+            IList<IList<int>> result = new List<IList<int>>();
+
+            IList<int> temp = new List<int>();
+            foreach (var item in set1)
+            {
+                if (!set2.Contains(item))
+                {
+                    temp.Add(item);
+                }
+            }
+            result.Add(temp);
+            temp = new List<int>();
+            foreach (var item in set2)
+            {
+                if (!set1.Contains(item))
+                {
+                    temp.Add(item);
+                }
+            }
+            result.Add(temp);
+
+            //List<int> list1 = new List<int>();
+            //List<int> list2 = new List<int>();
+
+            //list1 = set1.Except(set2).ToList();
+            //list2 = set2.Except(set1).ToList();
+
+            //result.Add(list1);
+            //result.Add(list2);
+            return result;
+        }
+    }
+}
