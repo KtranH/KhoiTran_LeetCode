7023d3e348a056881e2ccc43c890f7956bfd56ee
KtranH <87740741+KtranH@users.noreply.github.com>
2025-02-16 20:39:32 +0700

    Solved Unique Number of Occurrences

---
+++ C_Sharp/Unique Number of Occurrences/Unique Number of Occurrences/Program.cs
@@ -0,0 +1,33 @@
+﻿//Example 1:
+
+//Input: arr = [1, 2, 2, 1, 1, 3]
+//Output: true
+//Explanation: The value 1 has 3 occurrences, 2 has 2 and 3 has 1. No two values have the same number of occurrences.
+//Example 2:
+
+//Input: arr = [1, 2]
+//Output: false
+//Example 3:
+
+//Input: arr = [-3, 0, 1, -3, 1, 1, 1, -3, 10, 0]
+//Output: true
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unique_Number_of_Occurrences
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 1, 2 };
+            Solution solution = new Solution();
+            Console.WriteLine(solution.solve_unique_number_of_occurrences(nums));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C_Sharp/Unique Number of Occurrences/Unique Number of Occurrences/Solution.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unique_Number_of_Occurrences
+{
+    public class Solution
+    {
+        public bool solve_unique_number_of_occurrences(int[] nums)
+        {
+            Dictionary<int,int> hashmap = new Dictionary<int,int>();
+            foreach (int num in nums)
+            {
+                if (hashmap.ContainsKey(num))
+                {
+                    hashmap[num]++;
+                }
+                else
+                {
+                    hashmap[num] = 1;
+                }
+            }
+            HashSet<int> hashset = new HashSet<int>();
+            foreach(int value in hashmap.Values)
+            {
+                hashset.Add(value);
+            }    
+            if(hashset.Count != hashmap.Count) return false;
+            return true;
+        }
+    }
+}
