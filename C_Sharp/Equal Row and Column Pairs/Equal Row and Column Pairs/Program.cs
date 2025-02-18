f42ba3135fd589763ae01be590514e09dd023161
KtranH <87740741+KtranH@users.noreply.github.com>
2025-02-18 19:07:43 +0700

    Solved Equal Row and Column Pairs

---
+++ C_Sharp/Equal Row and Column Pairs/Equal Row and Column Pairs/Program.cs
@@ -0,0 +1,38 @@
+﻿
+
+//Example 1:
+
+
+//Input: grid = [[3, 2, 1], [1, 7, 6], [2, 7, 7]]
+//Output: 1
+//Explanation: There is 1 equal row and column pair:
+//-(Row 2, Column 1): [2,7,7]
+//Example 2:
+
+
+//Input: grid = [[3, 1, 2, 2], [1, 4, 4, 5], [2, 4, 2, 2], [2, 4, 2, 2]]
+//Output: 3
+//Explanation: There are 3 equal row and column pairs:
+//-(Row 0, Column 0): [3, 1, 2, 2]
+//- (Row 2, Column 2): [2, 4, 2, 2]
+//- (Row 3, Column 2): [2,4,2,2]
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Equal_Row_and_Column_Pairs
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] grid = new int[][] { new int[] { 13, 13 }, new int[] { 13, 13 }};
+            Solution solution = new Solution();
+            Console.WriteLine(solution.solve_equal_row_and_column_pairs(grid));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C_Sharp/Equal Row and Column Pairs/Equal Row and Column Pairs/Solution.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Equal_Row_and_Column_Pairs
+{
+    public class Solution
+    {
+        public int solve_equal_row_and_column_pairs(int[][] grid)
+        {
+            Dictionary<string, int> hashmap = new Dictionary<string, int>();
+            int count = 0;
+            int length_grid = grid.Length;
+
+            for (int i = 0; i < length_grid; i++)
+            {
+                string rowKey = string.Join(",", grid[i]);
+                if (hashmap.ContainsKey(rowKey))
+                    hashmap[rowKey] += 1;
+                else
+                    hashmap[rowKey] = 1;
+            }
+
+            for (int i = 0; i < length_grid; i++)
+            {
+                int[] column = new int[length_grid];
+                for (int j = 0; j < length_grid; j++)
+                    column[j] = grid[j][i];
+
+                string colKey = string.Join(",", column);
+                if (hashmap.ContainsKey(colKey))
+                    count += hashmap[colKey];
+            }
+
+            return count;
+        }
+    }
+}
