c35e45cfb56735153cdbafe4acc82c9a737d5b76
KtranH <87740741+KtranH@users.noreply.github.com>
2025-02-19 20:15:25 +0700

    Solved Removing Stars From a String

---
+++ C_Sharp/Removing Stars From a String/Removing Stars From a String/Solution.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Removing_Stars_From_a_String
+{
+    public class Solution
+    {
+        public string solve_removing_stars_from_a_string(string s)
+        {
+            List<char> stack = new List<char>();
+            foreach(char c in s)
+            {
+                if (c == '*')
+                {
+                    if (stack.Count > 0) stack.RemoveAt(stack.Count - 1);
+                }
+                else
+                {
+                    stack.Add(c);
+                }
+            }
+            return string.Join("", stack);
+        }
+    }
+}
