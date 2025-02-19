7bdc3728c3e610e2fb4a1bb7425def01de6cf4e0
KtranH <87740741+KtranH@users.noreply.github.com>
2025-02-19 20:02:44 +0700

    Solved Removing Stars From a String

---
+++ Python/Removing Stars From a String/main.py
@@ -0,0 +1,32 @@
+"""
+Example 1:
+
+Input: s = "leet**cod*e"
+Output: "lecoe"
+Explanation: Performing the removals from left to right:
+- The closest character to the 1st star is 't' in "leet**cod*e". s becomes "lee*cod*e".
+- The closest character to the 2nd star is 'e' in "lee*cod*e". s becomes "lecod*e".
+- The closest character to the 3rd star is 'd' in "lecod*e". s becomes "lecoe".
+There are no more stars, so we return "lecoe".
+Example 2:
+
+Input: s = "erase*****"
+Output: ""
+Explanation: The entire string is removed, so we return an empty string.
+"""
+
+s = "leet**cod*e"
+
+def solve_removing_stars_from_a_string(s):
+    stack = []
+    result  = ""
+    for char in s:
+        if char == '*':
+            stack.pop()
+        else:
+            stack.append(char)
+    result = ''.join(stack)
+
+    return result  
+
+print(solve_removing_stars_from_a_string(s))
