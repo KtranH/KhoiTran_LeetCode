d4cf3abf278955f1fff3824ed658bcafb8101b35
KtranH <87740741+KtranH@users.noreply.github.com>
2025-02-21 20:03:39 +0700

    Solved Decode String

---
+++ C_Sharp/Decode String/Decode String/Program.cs
@@ -0,0 +1,32 @@
+﻿//Example 1:
+
+//Input: s = "3[a]2[bc]"
+//Output: "aaabcbc"
+//Example 2:
+
+//Input: s = "3[a2[c]]"
+//Output: "accaccacc"
+//Example 3:
+
+//Input: s = "2[abc]3[cd]ef"
+//Output: "abcabccdcdcdef"
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decode_String
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "3[a]2[bc]";
+            Solution solution = new Solution();
+            Console.WriteLine(solution.solve_decode_string(s));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C_Sharp/Decode String/Decode String/Solution.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decode_String
+{
+    public class Solution
+    {
+        public string solve_decode_string(string s)
+        {
+            string current_string = "";
+            int current_number = 0;
+            Stack<(int, string)> stack = new Stack<(int, string)>();
+
+            foreach (char c in s)
+            {
+                if (Char.IsDigit(c))
+                {
+                    current_number = current_number * 10 + (c - '0'); 
+                }
+                else if (c == '[')
+                {
+                    stack.Push((current_number, current_string));
+                    current_number = 0;
+                    current_string = "";
+                }
+                else if (c == ']')
+                {
+                    var (num, prev_string) = stack.Pop();
+                    current_string = prev_string + string.Concat(Enumerable.Repeat(current_string, num)); 
+                }
+                else
+                {
+                    current_string += c;
+                }
+            }
+            return current_string;
+        }
+    }
+}
