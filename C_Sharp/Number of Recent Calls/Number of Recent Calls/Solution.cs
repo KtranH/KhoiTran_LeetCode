68505250138f05b7c373303d9a364981c2f2772f
KtranH <87740741+KtranH@users.noreply.github.com>
2025-02-22 19:00:02 +0700

    Solved Number of Recent Calls

---
+++ C_Sharp/Number of Recent Calls/Number of Recent Calls/Solution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Number_of_Recent_Calls
+{
+    public class Solution
+    {
+        public Queue<int> queue;
+        public Solution()
+        {
+            queue = new Queue<int>();
+        }
+        public int Ping(int t) {
+            this.queue.Enqueue(t);
+            while (this.queue.Peek() < t - 3000)
+            {
+                this.queue.Dequeue();
+            }
+            return this.queue.Count;
+        }
+    }
+}
