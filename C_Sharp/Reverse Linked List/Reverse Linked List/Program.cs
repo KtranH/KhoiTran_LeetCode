95d824c572c7583382e600030f7efdc7f7461869
KtranH <87740741+KtranH@users.noreply.github.com>
2025-02-27 19:28:36 +0700

    Solved Reverse Linked List

---
+++ C_Sharp/Reverse Linked List/Reverse Linked List/Program.cs
@@ -0,0 +1,42 @@
+﻿//Example 1:
+
+
+//Input: head = [1, 2, 3, 4, 5]
+//Output: [5,4,3,2,1]
+//Example 2:
+
+
+//Input: head = [1, 2]
+//Output: [2,1]
+//Example 3:
+
+//Input: head = []
+//Output: []
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse_Linked_List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            s.append(1);
+            s.append(2);
+            s.append(3);
+            s.append(4);
+            s.append(5);
+            s.printList(s.head);
+            Console.WriteLine();
+            s.reverse();
+            s.printList(s.head);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C_Sharp/Reverse Linked List/Reverse Linked List/Solution.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse_Linked_List
+{
+    public class Node
+    {
+        public int val;
+        public Node next;
+        public Node(int data)
+        {
+            this.val = data;
+            this.next = null;
+        }
+    }
+
+    internal class Solution
+    {
+        public Node head;
+        public void append(int data)
+        {
+            Node newNode = new Node(data);
+            if (this.head == null)
+            {
+                this.head = newNode;
+                return;
+            }
+            Node last = this.head;
+            while (last.next != null)
+            {
+                last = last.next;
+            }
+            last.next = newNode;
+        }
+        public void printList(Node head)
+        {
+            Node curr = head;
+            while (curr != null)
+            {
+                Console.Write(curr.val + " ");
+                curr = curr.next;
+            }
+        }
+        public void reverse()
+        {
+            Node prev = null;
+            Node current = this.head;
+            Node nextNode = null;
+            while(current != null)
+            {
+                nextNode = current.next;
+                current.next = prev;
+                prev = current;
+                current = nextNode;
+            }    
+            this.head = prev;
+        }
+    }
+}
