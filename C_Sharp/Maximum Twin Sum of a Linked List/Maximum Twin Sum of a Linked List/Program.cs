1b886f6f88b76d3504cbe9def61db307577c1250
KtranH <87740741+KtranH@users.noreply.github.com>
2025-02-28 19:06:26 +0700

    Solved Maximum Twin Sum of a Linked List

---
+++ C_Sharp/Maximum Twin Sum of a Linked List/Maximum Twin Sum of a Linked List/Program.cs
@@ -0,0 +1,49 @@
+﻿//Example 1:
+
+
+//Input: head = [5, 4, 2, 1]
+//Output: 6
+//Explanation:
+//Nodes 0 and 1 are the twins of nodes 3 and 2, respectively. All have twin sum = 6.
+//There are no other nodes with twins in the linked list.
+//Thus, the maximum twin sum of the linked list is 6. 
+//Example 2:
+
+
+//Input: head = [4, 2, 2, 3]
+//Output: 7
+//Explanation:
+//The nodes with twins present in this linked list are:
+//-Node 0 is the twin of node 3 having a twin sum of 4 + 3 = 7.
+//- Node 1 is the twin of node 2 having a twin sum of 2 + 2 = 4.
+//Thus, the maximum twin sum of the linked list is max(7, 4) = 7. 
+//Example 3:
+
+
+//Input: head = [1, 100000]
+//Output: 100001
+//Explanation:
+//There is only one node with a twin in the linked list having twin sum of 1 + 100000 = 100001.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maximum_Twin_Sum_of_a_Linked_List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            s.append(5);
+            s.append(4);
+            s.append(2);
+            s.append(1);
+            Console.WriteLine(s.solve_maximum_twin_sum_of_a_linked_list());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C_Sharp/Maximum Twin Sum of a Linked List/Maximum Twin Sum of a Linked List/Solution.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maximum_Twin_Sum_of_a_Linked_List
+{
+    public class Node
+    {
+        public int val;
+        public Node next;
+        public Node(int val)
+        {
+            this.val = val;
+            this.next = null;
+        }
+    }
+    internal class Solution
+    {
+        public Node head = null;
+        public void append(int val)
+        {
+            Node newNode = new Node(val);
+            if (head == null)
+            {
+                head = newNode;
+                return;
+            }
+            Node last = head;
+            while (last.next != null)
+            {
+                last = last.next;
+            }
+            last.next = newNode;
+        }
+        public void display(Node head)
+        {
+            Node temp = head;
+            while (temp != null)
+            {
+                Console.Write(temp.val + " ");
+                temp = temp.next;
+            }
+            Console.WriteLine();
+        }
+        public Node findMiddle(Node slow_node)
+        {
+            Node slow = this.head;
+            Node fast = this.head;
+            while(fast != null && fast.next != null)
+            {
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+            return slow;
+        }
+        public Node reverse(Node slow_node)
+        {
+            Node prev = null;
+            Node curr = slow_node;
+            Node next = null;
+            while (curr != null)
+            {
+                next = curr.next;
+                curr.next = prev;
+                prev = curr;
+                curr = next;
+            }
+            return prev;
+        }
+        public int solve_maximum_twin_sum_of_a_linked_list()
+        {
+            Node prev = this.head;
+            Node after = reverse(findMiddle(this.head));
+            int max = 0;
+            while(after != null)
+            {
+                int temp_sum = prev.val + after.val;
+                if (temp_sum > max)
+                {
+                    max = temp_sum;
+                }
+                prev = prev.next;
+                after = after.next;
+            }
+            return max;
+        }
+    }
+}
