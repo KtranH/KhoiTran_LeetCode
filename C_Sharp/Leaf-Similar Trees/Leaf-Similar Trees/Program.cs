3f892ac4810d50ea6234886d7076f2a5861452e7
KtranH <87740741+KtranH@users.noreply.github.com>
2025-03-03 18:57:55 +0700

    Solved Leaf-Similar Trees

---
+++ C_Sharp/Leaf-Similar Trees/Leaf-Similar Trees/Program.cs
@@ -0,0 +1,31 @@
+﻿//Example 1:
+
+
+//Input: root1 = [3, 5, 1, 6, 2, 9, 8, null, null, 7, 4], root2 = [3, 5, 1, 6, 7, 4, 2, null, null, null, null, null, null, 9, 8]
+//Output: true
+//Example 2:
+
+
+//Input: root1 = [1, 2, 3], root2 = [1, 3, 2]
+//Output: false
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leaf_Similar_Trees
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            Node root1 = s.insert_level_order(new int?[] { 3, 5, 1, 6, 2, 9, 8, null, null, 7, 4 });
+            Node root2 = s.insert_level_order(new int?[] { 3, 5, 1, 6, 7, 4, 2, null, null, null, null, null, null, 9, 8 });
+            Console.WriteLine(s.solve_leaf_similar_trees(root1, root2));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C_Sharp/Leaf-Similar Trees/Leaf-Similar Trees/Solution.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leaf_Similar_Trees
+{
+    public class Node
+    {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node(int val) {
+            this.val = val;
+            this.left = this.right = null;
+        }
+    }
+    internal class Solution
+    {
+        public Node root = null;
+        public Node insert_level_order(int?[] values)
+        {
+            if(values.Length == 0)
+                return null;
+            Queue<Node> q = new Queue<Node>();
+            this.root = new Node(values[0].GetValueOrDefault());
+            int i = 1;
+            while (i < values.Length && q.Count != 0)
+            {
+                Node curr = q.Dequeue();
+                if (i < values.Length && values[i] != null)
+                {
+                    curr.left = new Node(values[i].GetValueOrDefault());
+                    q.Enqueue(curr.left);
+                }
+                i++;
+                if (i < values.Length && values[i] != null)
+                {
+                    curr.right = new Node(values[i].GetValueOrDefault());
+                    q.Enqueue(curr.right);
+                }
+                i++;
+            }    
+            return this.root;
+        }
+        public List<int> DFS(Node root)
+        {
+            if(root == null)
+                return new List<int>();
+            if(root.left == null && root.right == null)
+                return new List<int>() { root.val };
+            return DFS(root.left).Concat(DFS(root.right)).ToList();
+        }
+        public bool solve_leaf_similar_trees(Node root1, Node root2)
+        {
+            return DFS(root1).SequenceEqual(DFS(root2));
+        }
+    }
+}
