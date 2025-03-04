60b1b09c1d668964ac4d890387622a893a36600e
KtranH <87740741+KtranH@users.noreply.github.com>
2025-03-04 19:37:35 +0700

    Solved Count Good Nodes in Binary Tree

---
+++ C_Sharp/Count Good Nodes in Binary Tree/Count Good Nodes in Binary Tree/Program.cs
@@ -0,0 +1,44 @@
+﻿//Example 1:
+
+
+
+//Input: root = [3, 1, 4, 3, null, 1, 5]
+//Output: 4
+//Explanation: Nodes in blue are good.
+//Root Node (3) is always a good node.
+//Node 4 -> (3,4) is the maximum value in the path starting from the root.
+//Node 5 -> (3,4,5) is the maximum value in the path
+//Node 3 -> (3,1,3) is the maximum value in the path.
+//Example 2:
+
+
+
+//Input: root = [3, 3, null, 4, 2]
+//Output: 3
+//Explanation: Node 2-> (3, 3, 2) is not good, because "3" is higher than it.
+//Example 3:
+
+//Input: root = [1]
+//Output: 1
+//Explanation: Root is considered as good.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Count_Good_Nodes_in_Binary_Tree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            int?[] values = {3, 1, 4, 3, null, 1, 5};
+            Node root = solution.insert_level_order(values);
+            Console.WriteLine(solution.solve_count_good_nodes_in_binary_tree(root));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C_Sharp/Count Good Nodes in Binary Tree/Count Good Nodes in Binary Tree/Solution.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Count_Good_Nodes_in_Binary_Tree
+{
+    public class Node
+    {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node(int val) {
+            this.val = val;
+            this.left = null;
+            this.right = null;
+        }
+    }
+    internal class Solution
+    {
+        public Node root = null;
+        public Node insert_level_order(int?[] values)
+        {
+            if(values.Length == 0)
+                return null;
+            Queue<Node> nodes = new Queue<Node>();
+            this.root = new Node(values[0].Value);
+            nodes.Enqueue(this.root);
+            int i = 1;
+            while(nodes.Count > 0 && i < values.Length)
+            {
+                Node current = nodes.Dequeue();
+                if (values[i].HasValue)
+                {
+                    current.left = new Node(values[i].Value);
+                    nodes.Enqueue(current.left);
+                }
+                i++;
+                if(i < values.Length && values[i].HasValue)
+                {
+                    current.right = new Node(values[i].Value);
+                    nodes.Enqueue(current.right);
+                }
+                i++;
+            }
+            return this.root;
+        }
+        public int DFS(Node root, int max_val)
+        {
+            int count = 0;
+            if(root == null)
+                return 0;
+            if (root.val >= max_val)
+            {
+                count = 1;
+                max_val = root.val;
+            }
+            return count + DFS(root.left, max_val) + DFS(root.right, max_val); 
+        }
+        public int solve_count_good_nodes_in_binary_tree(Node root)
+        {
+            return DFS(root, root.val);
+        }
+    }
+}
