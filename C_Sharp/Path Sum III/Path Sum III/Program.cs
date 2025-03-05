f7691d1191a7528ad090cf294d0fe500be7364b0
KtranH <87740741+KtranH@users.noreply.github.com>
2025-03-05 21:08:40 +0700

    Solved Path Sum III

---
+++ C_Sharp/Path Sum III/Path Sum III/Program.cs
@@ -0,0 +1,30 @@
+﻿//Example 1:
+
+
+//Input: root = [10, 5, -3, 3, 2, null, 11, 3, -2, null, 1], targetSum = 8
+//Output: 3
+//Explanation: The paths that sum to 8 are shown.
+//Example 2:
+
+//Input: root = [5, 4, 8, 11, null, 13, 4, 7, 2, null, null, 5, 1], targetSum = 22
+//Output: 3
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Path_Sum_III
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            Node root = solution.insert_level_order(new int?[] { 5, 4, 8, 11, null, 13, 4, 7, 2, null, null, 5, 1 });
+            Console.WriteLine(solution.solve_path_sum_iii(root, 22));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C_Sharp/Path Sum III/Path Sum III/Solution.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Path_Sum_III
+{
+    public class Node
+    {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node(int val)
+        {
+            this.val = val;
+            this.left = null;
+            this.right = null;
+        }
+    }
+    internal class Solution
+    {
+        public Node root = null;
+        public Node insert_level_order(int?[] values)
+        {
+            if(values.Length == 0) return null;
+            this.root = new Node(values[0].Value);
+            Queue<Node> queue = new Queue<Node>();
+            queue.Enqueue(this.root);
+            int i = 0;
+            while(queue.Count > 0 && i < values.Length)
+            {
+                Node node = queue.Dequeue();
+                if(values[i] != null)
+                {
+                    node.left = new Node(values[i].Value);
+                    queue.Enqueue(node.left);
+                }
+                i++;
+                if(i < values.Length && values[i] != null)
+                {
+                    node.right = new Node(values[i].Value);
+                    queue.Enqueue(node.right);
+                }
+                i++;
+            }    
+            return this.root;
+        }
+        public int DFS(Node node, long currentSum, long targetSum, Dictionary<long, int> hashMap_val)
+        {
+            if(node == null) return 0;
+            int count = 0;
+            currentSum += node.val;
+            long checkHashMap = currentSum - targetSum;
+            if (hashMap_val.ContainsKey(checkHashMap)) {
+                count = hashMap_val[checkHashMap];
+            }
+            if(hashMap_val.ContainsKey(currentSum)) {
+                hashMap_val[currentSum] += 1;
+            } else {
+                hashMap_val.Add(currentSum, 1);
+            }
+            count += DFS(node.left, currentSum, targetSum, hashMap_val);
+            count += DFS(node.right, currentSum, targetSum, hashMap_val);
+            hashMap_val[currentSum] -= 1;
+            return count;
+        }
+        public int solve_path_sum_iii(Node root, long targetSum) {
+            Dictionary<long, int> hashMap_val = new Dictionary<long, int>();
+            hashMap_val.Add(0, 1);
+            return DFS(root, 0, targetSum, hashMap_val);
+        }
+    }
+}
