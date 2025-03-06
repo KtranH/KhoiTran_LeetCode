0bcec4d5043893d67f2fb5eb681b0805f7ab10d0
KtranH <87740741+KtranH@users.noreply.github.com>
2025-03-06 19:42:33 +0700

    Solved Longest ZigZag Path in a Binary Tree

---
+++ C_Sharp/Longest ZigZag Path in a Binary Tree/Longest ZigZag Path in a Binary Tree/Program.cs
@@ -0,0 +1,36 @@
+﻿//Example 1:
+
+
+//Input: root = [1, null, 1, 1, 1, null, null, 1, 1, null, 1, null, null, null, 1]
+//Output: 3
+//Explanation: Longest ZigZag path in blue nodes (right -> left -> right).
+//Example 2:
+
+
+//Input: root = [1, 1, 1, null, 1, null, null, 1, 1, null, 1]
+//Output: 4
+//Explanation: Longest ZigZag path in blue nodes (left -> right -> left -> right).
+//Example 3:
+
+//Input: root = [1]
+//Output: 0
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Longest_ZigZag_Path_in_a_Binary_Tree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            Node root = solution.insert_level_order(new int?[] { 5, 4, 8, 11, null, 13, 4, 7, 2, null, null, 5, 1 });
+            Console.WriteLine(solution.solve_longest_zigzag_path_in_a_binary_tree(root));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C_Sharp/Longest ZigZag Path in a Binary Tree/Longest ZigZag Path in a Binary Tree/Solution.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Longest_ZigZag_Path_in_a_Binary_Tree
+{
+    public class Node
+    {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node(int val) {
+            this.val = val;
+            this.left = this.right = null;
+        }
+    }
+    internal class Solution
+    {
+        public Node root = null;
+        public int max_length = 0;
+        public Node insert_level_order(int?[] values)
+        {
+            if(values.Length == 0) { return null; }
+            Queue<Node> queue = new Queue<Node>();
+            this.root = new Node(values[0].Value);
+            queue.Enqueue(this.root);
+            int i = 1;
+            while(i < values.Length && queue.Count > 0)
+            {
+                Node node = queue.Dequeue();
+                if(i < values.Length && values[i] != null)
+                {
+                    node.left = new Node(values[i].Value);
+                    queue.Enqueue(node.left);
+                }
+                i++;
+                if(i < values.Length && values[i] != null)
+                {
+                    node.right = new Node(values[i].Value);
+                    queue.Enqueue(node.right);
+                }
+                i++;
+            }
+            return this.root;
+        }
+        public void DFS(Node node, string direction, int length)
+        {
+            if(node == null) return;
+            if(length > this.max_length) { this.max_length = length; }
+            if(direction == "left")
+            {
+                DFS(node.left, "right", length + 1);
+                DFS(node.right, "left", 1);
+            }
+            else
+            {
+                DFS(node.right, "left", length + 1);
+                DFS(node.left, "right", 1);
+            }
+        }
+        public int solve_longest_zigzag_path_in_a_binary_tree(Node root)
+        {
+            this.DFS(root, "left", 0);
+            this.DFS(root, "right", 0);
+            return this.max_length;
+        }
+    }
+}
