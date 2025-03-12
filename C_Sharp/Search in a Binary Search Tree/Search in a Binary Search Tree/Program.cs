e172957cc23716b9fca3f0b4d2c33b3f2d1a16bc
KtranH <87740741+KtranH@users.noreply.github.com>
2025-03-12 14:05:21 +0700

    Solved Search in a Binary Search Tree

---
+++ C_Sharp/Search in a Binary Search Tree/Search in a Binary Search Tree/Program.cs
@@ -0,0 +1,34 @@
+﻿//Example 1:
+
+
+//Input: root = [4, 2, 7, 1, 3], val = 2
+//Output: [2,1,3]
+//Example 2:
+
+
+//Input: root = [4, 2, 7, 1, 3], val = 5
+//Output: []
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Search_in_a_Binary_Search_Tree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            int[] values = { 1, 2, 3, 4, 5 };
+            foreach (int value in values)
+            {
+                solution.insert(value);
+            }    
+            Console.WriteLine(solution.solve_search_in_a_search_tree(2).val);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C_Sharp/Search in a Binary Search Tree/Search in a Binary Search Tree/Solution.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Search_in_a_Binary_Search_Tree
+{
+    public class Node
+    {
+        public int val { get; set; }
+        public Node left { get; set; }
+        public Node right { get; set; }
+        public Node(int val) {
+            this.val = val;
+            this.left = null;
+            this.right = null;
+        }
+    }
+    internal class Solution
+    {
+        public Node root = null;
+        public void insert(int val)
+        {
+            if(this.root == null)
+            {
+                this.root = new Node(val);
+            }    
+            else
+            {
+                _insert_recursive(this.root, val);
+            }    
+        }
+        public void _insert_recursive(Node node, int val)
+        {
+            if (node.val > val)
+            {
+                if (node.left == null)
+                {
+                    node.left = new Node(val);
+                }
+                else
+                {
+                    _insert_recursive(node.left, val);
+                }
+            }
+            else
+            {
+                if (node.right == null)
+                {
+                    node.right = new Node(val);
+                }
+                else
+                {
+                    _insert_recursive(node.right, val);
+                }
+            }
+        }
+        public Node solve_search_in_a_search_tree(int val)
+        {
+            while(this.root != null && this.root.val != val)
+            {
+                if (this.root.val > val)
+                {
+                    this.root = this.root.left;
+                }
+                else
+                {
+                    this.root = this.root.right;
+                }
+            }    
+            return this.root;
+        }
+    }
+}
