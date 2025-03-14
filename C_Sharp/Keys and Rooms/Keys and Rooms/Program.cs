76bc9e0ca17a1f70a447655fca899214d2c9a383
KtranH <87740741+KtranH@users.noreply.github.com>
2025-03-14 13:36:56 +0700

    Solved Keys and Rooms

---
+++ C_Sharp/Keys and Rooms/Keys and Rooms/Program.cs
@@ -0,0 +1,34 @@
+﻿//Example 1:
+
+//Input: rooms = [[1], [2], [3], []]
+//Output: true
+//Explanation:
+//We visit room 0 and pick up key 1.
+//We then visit room 1 and pick up key 2.
+//We then visit room 2 and pick up key 3.
+//We then visit room 3.
+//Since we were able to visit every room, we return true.
+//Example 2:
+
+//Input: rooms = [[1, 3], [3, 0, 1], [2], [0]]
+//Output: false
+//Explanation: We can not enter room number 2 since the only key that unlocks it is in that room.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Keys_and_Rooms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            Console.Write(solution.solve_keys_and_rooms());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C_Sharp/Keys and Rooms/Keys and Rooms/Solution.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Keys_and_Rooms
+{
+    internal class Solution
+    {
+        public bool solve_keys_and_rooms()
+        {
+            List<List<int>> rooms = new List<List<int>>() { new List<int>() { 1 }, new List<int>() { 2 }, new List<int>() { 3 }, new List<int>() { } };
+            bool[] visited = new bool[rooms.Count];
+            this.dfs(rooms, visited, 0);
+            foreach(bool room in visited)
+            {
+                if(!room)
+                {
+                    return false;
+                }
+            }    
+            return true;
+        }
+        public void dfs(List<List<int>> rooms, bool[] visited, int key)
+        {
+            if(visited[key])
+            {
+                return;
+            }    
+            visited[key] = true;
+            foreach(int room in rooms[key])
+            {
+                dfs(rooms, visited, room);
+            }
+        }
+    }
+}
