556ac6b7e71510fceef189cb8e320c31d48fa803
KtranH <87740741+KtranH@users.noreply.github.com>
2025-03-17 11:46:13 +0700

    Solved Number of Provinces

---
+++ C_Sharp/Number of Provinces/Number of Provinces/Program.cs
@@ -0,0 +1,29 @@
+﻿//Example 1:
+
+
+//Input: isConnected = [[1, 1, 0], [1, 1, 0], [0, 0, 1]]
+//Output: 2
+//Example 2:
+
+
+//Input: isConnected = [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
+//Output: 3
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Number_of_Provinces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            Console.WriteLine(solution.solve_number_of_provinces());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C_Sharp/Number of Provinces/Number of Provinces/Solution.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Number_of_Provinces
+{
+    internal class Solution
+    {
+        public void DFS(int[][] isConnected, bool[] bools, int index)
+        {
+            if(bools[index])
+            {
+                return;
+            }    
+            bools[index] = true;
+            for (int i = 0; i < isConnected.Length; i++)
+            {
+                if (isConnected[index][i] == 1)
+                {
+                    DFS(isConnected, bools, i);
+                }
+            }
+        }
+        public int solve_number_of_provinces()
+        {
+            int[][] isConnected = new int[][] { new int[] { 1, 1, 0 }, new int[] { 1, 1, 0 }, new int[] { 0, 0, 1 } };
+            bool[] bools = new bool[isConnected.Length];
+            int count = 0;
+            for (int i = 0; i < isConnected.Length; i++)
+            {
+                if (!bools[i])
+                {
+                    count++;
+                    DFS(isConnected, bools, i);
+                }
+            }    
+            return count;
+        }
+    }
+}
