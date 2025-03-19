e2074b24c4e651a20008e7372ff533e11e010646
KtranH <87740741+KtranH@users.noreply.github.com>
2025-03-19 11:55:36 +0700

    Solved Evaluate Division

---
+++ C_Sharp/Evaluate Division/Evaluate Division/Program.cs
@@ -0,0 +1,41 @@
+﻿//Example 1:
+
+//Input: equations = [["a", "b"], ["b", "c"]], values = [2.0, 3.0], queries = [["a", "c"], ["b", "a"], ["a", "e"], ["a", "a"], ["x", "x"]]
+//Output: [6.00000,0.50000,-1.00000,1.00000,-1.00000]
+//Explanation:
+//Given: a / b = 2.0, b / c = 3.0
+//queries are: a / c = ?, b / a = ?, a / e = ?, a / a = ?, x / x = ?
+//return: [6.0, 0.5, -1.0, 1.0, -1.0 ]
+//note: x is undefined => -1.0
+//Example 2:
+
+//Input: equations = [["a", "b"], ["b", "c"], ["bc", "cd"]], values = [1.5, 2.5, 5.0], queries = [["a", "c"], ["c", "b"], ["bc", "cd"], ["cd", "bc"]]
+//Output: [3.75000,0.40000,5.00000,0.20000]
+
+//[1,0] does not count as an exit since it is the entrance cell.
+//Initially, you are at the entrance cell [1, 0].
+//- You can reach[1, 2] by moving 2 steps right.
+//Thus, the nearest exit is [1,2], which is 2 steps away.
+//Example 3:
+
+//Input: equations = [["a", "b"]], values = [0.5], queries = [["a", "b"], ["b", "a"], ["a", "c"], ["x", "y"]]
+//Output: [0.50000,2.00000,-1.00000,-1.00000]
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evaluate_Division
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            Console.WriteLine(solution.solve_evaluate_division());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C_Sharp/Evaluate Division/Evaluate Division/Solution.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evaluate_Division
+{
+    internal class Solution
+    {
+        public double DFS(string node, string target, double product, Dictionary<string, IList<string>> graph, HashSet<string> visited, Dictionary<string, double> values_direct)
+        {
+            if(!graph.ContainsKey(node) || !graph.ContainsKey(target))
+            {
+                return -1.0;
+            }    
+            if(node == target)
+            {
+                return product;
+            }    
+            visited.Add(node);
+            foreach(string neighbor in graph[node])
+            {
+                if(visited.Contains(neighbor))
+                {
+                    continue;
+                }    
+                double result = DFS(neighbor, target, product * values_direct[node + "," + neighbor], graph, visited, values_direct);
+                if(result != -1)
+                {
+                    return result;
+                }    
+            }
+            return -1.0;
+        }
+        public double[] solve_evaluate_division()
+        {
+            IList<IList<string>> equations = new List<IList<string>>() { new List<string>() { "a", "b" }, new List<string>() { "b", "c" } };
+            double[] values = new double[] { 2.0, 3.0 };
+            IList<IList<string>> queries = new List<IList<string>>() { new List<string>() { "a", "c" }, new List<string>() { "b", "a" }, new List<string>() { "a", "e" }, new List<string>() { "a", "a" }, new List<string>() { "x", "x" } };
+            double[] doubles = new double[queries.Count()];
+            Dictionary<string, IList<string>> graph = new Dictionary<string, IList<string>>();
+            Dictionary<string, double> values_direct = new Dictionary<string, double>();
+            for (int i = 0; i < equations.Count(); i++)
+            {
+                string[] equation = equations[i].ToArray();
+                if (!graph.ContainsKey(equation[0]))
+                {
+                    graph[equation[0]] = new List<string>();
+                }
+                if (!graph.ContainsKey(equation[1]))
+                {
+                    graph[equation[1]] = new List<string>();
+                }
+                graph[equation[0]].Add(equation[1]);
+                graph[equation[1]].Add(equation[0]);
+                values_direct[equation[0] + "," + equation[1]] = values[i];
+                values_direct[equation[1] +  "," + equation[0]] = 1 / values[i];
+            }    
+            for (int i = 0; i < queries.Count(); i++)
+            {
+                string[] query = queries[i].ToArray();
+                HashSet<string> visited = new HashSet<string>();
+                doubles[i] = DFS(query[0], query[1], 1.0, graph, visited, values_direct);
+            }
+            return doubles;
+        }
+    }
+}
