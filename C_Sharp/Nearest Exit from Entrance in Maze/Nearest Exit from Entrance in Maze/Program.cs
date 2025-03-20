c17a3864bda1764ee1fae43261ac90d81b9137ee
KtranH <87740741+KtranH@users.noreply.github.com>
2025-03-20 13:21:09 +0700

    Solved Nearest Exit from Entrance in Maze

---
+++ C_Sharp/Nearest Exit from Entrance in Maze/Nearest Exit from Entrance in Maze/Program.cs
@@ -0,0 +1,46 @@
+﻿/*Example 1:
+
+
+Input: maze = [["+", "+", ".", "+"], [".", ".", ".", "+"], ["+", "+", "+", "."]], entrance = [1, 2]
+Output: 1
+Explanation: There are 3 exits in this maze at [1,0], [0,2], and[2, 3].
+Initially, you are at the entrance cell [1, 2].
+- You can reach[1, 0] by moving 2 steps left.
+- You can reach [0,2] by moving 1 step up.
+It is impossible to reach [2,3] from the entrance.
+Thus, the nearest exit is [0,2], which is 1 step away.
+Example 2:
+
+
+Input: maze = [["+", "+", "+"], [".", ".", "."], ["+", "+", "+"]], entrance = [1, 0]
+Output: 2
+Explanation: There is 1 exit in this maze at [1, 2].
+[1, 0] does not count as an exit since it is the entrance cell.
+Initially, you are at the entrance cell [1, 0].
+- You can reach[1, 2] by moving 2 steps right.
+Thus, the nearest exit is [1,2], which is 2 steps away.
+Example 3:
+
+
+Input: maze = [[".", "+"]], entrance = [0, 0]
+Output: -1
+Explanation: There are no exits in this maze.*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nearest_Exit_from_Entrance_in_Maze
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            Console.WriteLine(solution.solve_nearest_exit());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C_Sharp/Nearest Exit from Entrance in Maze/Nearest Exit from Entrance in Maze/Solution.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nearest_Exit_from_Entrance_in_Maze
+{
+    internal class Solution
+    {
+        public int BFS(char[][] maze, int[] entrance, HashSet<(int, int)> visited)
+        {
+            Queue<(int, int, int)> queue = new Queue<(int, int, int)>();
+            queue.Enqueue((entrance[0], entrance[1], 0));
+            visited.Add((entrance[0], entrance[1]));
+            while (queue.Count > 0)
+            {
+                (int x, int y, int dist) = queue.Dequeue();
+                if ((x == 0 || x == maze.Length - 1 || y == 0 || y == maze[0].Length - 1) && ((x, y) != (entrance[0], entrance[1])))
+                {
+                    return dist;
+                }
+                foreach((int dx, int dy) in new (int, int)[] { (-1, 0), (1, 0), (0, -1), (0, 1) })
+                {
+                    int nx = x + dx, ny = y + dy;
+                    if(0 <= nx && nx < maze.Length && 0 <= ny && ny < maze[0].Length && maze[nx][ny] == '.' && !visited.Contains((nx, ny)))
+                    {
+                        visited.Add((nx, ny));
+                        queue.Enqueue((nx, ny, dist + 1));
+                    }    
+                }    
+            }    
+            return -1;
+        }
+        public int solve_nearest_exit()
+        {
+            char[][] maze = new char[][] { new char[] { '+', '+', '+', '+' }, new char[] { '+', '.', '.', '+' }, new char[] { '+', '+', '+', '+' } };
+            int[] entrance = new int[] { 1, 2 };
+            HashSet<(int, int)> visited = new HashSet<(int, int)>();
+            return BFS(maze, entrance, visited);
+        }
+    }
+}
