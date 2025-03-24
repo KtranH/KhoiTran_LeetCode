d7605318cba1e5277737e5bec6a4b063ca0a0dc7
KtranH <87740741+KtranH@users.noreply.github.com>
2025-03-24 13:17:46 +0700

    Solved Kth Largest Element in an Array

---
+++ C_Sharp/Kth Largest Element in an Array/Kth Largest Element in an Array/Program.cs
@@ -0,0 +1,28 @@
+﻿//Example 1:
+
+//Input: nums = [3, 2, 1, 5, 6, 4], k = 2
+//Output: 5
+//Example 2:
+
+//Input: nums = [3, 2, 3, 1, 2, 4, 5, 5, 6], k = 4
+//Output: 4
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kth_Largest_Element_in_an_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            Console.WriteLine(solution.solve_kth_largest_element_in_an_array(new int[] { 3, 2, 1, 5, 6, 4 }, 2));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C_Sharp/Kth Largest Element in an Array/Kth Largest Element in an Array/Solution.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kth_Largest_Element_in_an_Array
+{
+    internal class Solution
+    {
+        /*static int QuickSelect(int[] nums, int left, int right, int k)
+        {
+            int pivotIndex = Partition(nums, left, right);
+
+            if (pivotIndex == k)
+                return nums[pivotIndex]; 
+
+            if (pivotIndex < k)
+                return QuickSelect(nums, pivotIndex + 1, right, k); 
+
+            return QuickSelect(nums, left, pivotIndex - 1, k);  
+        }
+
+        static int Partition(int[] nums, int left, int right)
+        {
+            int pivot = nums[right]; 
+            int i = left; 
+
+            for (int j = left; j < right; j++)
+            {
+                if (nums[j] >= pivot)
+                {
+                    (nums[i], nums[j]) = (nums[j], nums[i]); 
+                    i++;
+                }
+            }
+
+            (nums[i], nums[right]) = (nums[right], nums[i]); 
+            return i;
+        }
+        public int solve_kth_largest_element_in_an_array(int[] nums, int k)
+        {
+            return QuickSelect(nums, 0, nums.Length - 1, k - 1);
+        }*/
+        static void Heapify(int[] heap, int n, int i)
+        {
+            int smallest = i;
+            int left = 2 * i + 1;
+            int right = 2 * i + 2;
+
+            if (left < n && heap[left] < heap[smallest])
+                smallest = left;
+
+            if (right < n && heap[right] < heap[smallest])
+                smallest = right;
+
+            if (smallest != i)
+            {
+                int swap = heap[i];
+                heap[i] = heap[smallest];
+                heap[smallest] = swap;
+
+                Heapify(heap, n, smallest);
+            }
+        }
+        public int solve_kth_largest_element_in_an_array(int[] nums, int k)
+        {
+            int[] heap = new int[k];
+
+            for (int i = 0; i < k; i++)
+            {
+                heap[i] = nums[i];
+            }
+
+            for (int i = k / 2 - 1; i >= 0; i--)
+            {
+                Heapify(heap, k, i);
+            }
+
+            for (int i = k; i < nums.Length; i++)
+            {
+                if (nums[i] > heap[0])
+                {
+                    heap[0] = nums[i];
+                    Heapify(heap, k, 0);
+                }
+            }
+            return heap[0];
+        }
+    }
+}
