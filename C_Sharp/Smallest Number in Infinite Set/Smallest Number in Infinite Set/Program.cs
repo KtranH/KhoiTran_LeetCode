be76b7b8cc49e7456f1c524dd3627881ac69909b
KtranH <87740741+KtranH@users.noreply.github.com>
2025-03-25 10:47:49 +0700

    Solved Smallest Number in Infinite Set

---
+++ C_Sharp/Smallest Number in Infinite Set/Smallest Number in Infinite Set/Program.cs
@@ -0,0 +1,36 @@
+﻿//Example 1:
+
+//Input
+//["SmallestInfiniteSet", "addBack", "popSmallest", "popSmallest", "popSmallest", "addBack", "popSmallest", "popSmallest", "popSmallest"]
+//[[], [2], [], [], [], [1], [], [], []]
+//Output
+//[null, null, 1, 2, 3, null, 1, 4, 5]
+
+//Explanation
+//SmallestInfiniteSet smallestInfiniteSet = new SmallestInfiniteSet();
+//smallestInfiniteSet.addBack(2);    // 2 is already in the set, so no change is made.
+//smallestInfiniteSet.popSmallest(); // return 1, since 1 is the smallest number, and remove it from the set.
+//smallestInfiniteSet.popSmallest(); // return 2, and remove it from the set.
+//smallestInfiniteSet.popSmallest(); // return 3, and remove it from the set.
+//smallestInfiniteSet.addBack(1);    // 1 is added back to the set.
+//smallestInfiniteSet.popSmallest(); // return 1, since 1 was added back to the set and
+//                                   // is the smallest number, and remove it from the set.
+//smallestInfiniteSet.popSmallest(); // return 4, and remove it from the set.
+//smallestInfiniteSet.popSmallest(); // return 5, and remove it from the set.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Smallest_Number_in_Infinite_Set
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C_Sharp/Smallest Number in Infinite Set/Smallest Number in Infinite Set/Solution.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Smallest_Number_in_Infinite_Set
+{
+    internal class Solution
+    {
+        public void solve_smallest_number_in_infinite_set()
+        {
+            SortedSet<int> heap = new SortedSet<int>();
+            int smallest = 1;
+            HashSet<int> addedBack = new HashSet<int>();
+        }
+        public int PopSmallest(SortedSet<int> heap, HashSet<int> addedBack, int smallest)
+        {
+            if(heap.Count != 0)
+            {
+                int small = heap.Min;
+                heap.Remove(small);
+                addedBack.Remove(small);
+                return small;
+            }
+            else
+            {
+                int small = smallest;
+                smallest++;
+                return small;
+            }    
+        }
+        public void AddBack(int num, SortedSet<int> heap, HashSet<int> addedBack, int smallest)
+        {
+            if(num < smallest && !addedBack.Contains(num))
+            {
+                heap.Add(num);
+                addedBack.Add(num);
+            }
+        }
+    }
+}
+ 
