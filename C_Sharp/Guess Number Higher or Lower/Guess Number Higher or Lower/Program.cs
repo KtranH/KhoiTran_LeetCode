4eaf9b5ffeee2352fbfee11c10654e37369e0e0d
KtranH <87740741+KtranH@users.noreply.github.com>
2025-03-28 11:39:08 +0700

    Solved Guess Number Higher or Lower

---
+++ C_Sharp/Guess Number Higher or Lower/Guess Number Higher or Lower/Program.cs
@@ -0,0 +1,28 @@
+﻿//Example 1:
+
+//Input: n = 10, pick = 6
+//Output: 6
+//Example 2:
+
+//Input: n = 1, pick = 1
+//Output: 1
+//Example 3:
+
+//Input: n = 2, pick = 1
+//Output: 1
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guess_Number_Higher_or_Lower
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ C_Sharp/Guess Number Higher or Lower/Guess Number Higher or Lower/Solution.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guess_Number_Higher_or_Lower
+{
+    internal class Solution
+    {
+        int guess(int num) { return 0; }
+        public int solve_guess_number(int n)
+        {
+            int left = 1;
+            int right = n;
+            while (left <= right)
+            {
+                int mid = left + (right - left) / 2;
+                int result = guess(mid);
+                if (result == 0)
+                {
+                    return mid;
+                }
+                else if (result == -1)
+                {
+                    right = mid - 1;
+                }
+                else
+                {
+                    left = mid + 1;
+                }
+            }
+            return -1;
+        }
+    }
+}
