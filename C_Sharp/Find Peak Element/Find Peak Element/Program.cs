e5819e964128ff54d1abf168eb7a0808b2dc35ef
KtranH <87740741+KtranH@users.noreply.github.com>
2025-04-01 12:38:58 +0700

    Solved Find Peak Element

---
+++ C_Sharp/Find Peak Element/Find Peak Element/Program.cs
@@ -0,0 +1,30 @@
+﻿//Example 1:
+
+//Input: nums = [1, 2, 3, 1]
+//Output: 2
+//Explanation: 3 is a peak element and your function should return the index number 2.
+//Example 2:
+
+//Input: nums = [1, 2, 1, 3, 5, 6, 4]
+//Output: 5
+//Explanation: Your function can return either index number 1 where the peak element is 2, or index number 5 where the peak element is 6.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Find_Peak_Element
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 2, 3, 1 };
+            Solution sol = new Solution();
+            Console.WriteLine(sol.solve_find_peak_element(nums));
+            Console.ReadLine();
+        }
+    }
+}
