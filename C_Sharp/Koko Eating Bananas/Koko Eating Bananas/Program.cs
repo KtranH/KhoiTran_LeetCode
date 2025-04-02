2c51d5b7915b152f9bbd4d633fbe1e121db86147
KtranH <87740741+KtranH@users.noreply.github.com>
2025-04-02 14:50:21 +0700

    Solved Koko Eating Bananas

---
+++ C_Sharp/Koko Eating Bananas/Koko Eating Bananas/Program.cs
@@ -0,0 +1,33 @@
+﻿//Example 1:
+
+//Input: piles = [3, 6, 7, 11], h = 8
+//Output: 4
+//Example 2:
+
+//Input: piles = [30, 11, 23, 4, 20], h = 5
+//Output: 30
+//Example 3:
+
+//Input: piles = [30, 11, 23, 4, 20], h = 6
+//Output: 23
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Koko_Eating_Bananas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            int[] piles = new int[] { 3, 6, 7, 11 };
+            int h = 8;
+            Console.WriteLine(solution.solve_koko_eating_piles(piles, h));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C_Sharp/Koko Eating Bananas/Koko Eating Bananas/Solution.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Koko_Eating_Bananas
+{
+    internal class Solution
+    {
+        public int solve_koko_eating_piles(int[] piles, int h) {
+            int left = 1;
+            int right = piles.Max();
+            while (left < right)
+            {
+                int mid = (left + right) / 2;
+                int hours = 0;
+                for (int i = 0; i < piles.Length; i++)
+                {
+                    hours += (piles[i] + mid - 1) / mid;
+                }
+                if (hours > h)
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid;
+                }
+            }
+            return left;
+        }
+    }
+}
