01e68d66a05771183284791c2149ceedac85d85e
KtranH <87740741+KtranH@users.noreply.github.com>
2025-04-07 08:26:19 +0700

    Solved N-th Tribonacci Number

---
+++ C_Sharp/N-th Tribonacci Number/N-th Tribonacci Number/Program.cs
@@ -0,0 +1,31 @@
+﻿//Example 1:
+
+//Input: n = 4
+//Output: 4
+//Explanation:
+//T_3 = 0 + 1 + 1 = 2
+//T_4 = 1 + 1 + 2 = 4
+//Example 2:
+
+//Input: n = 25
+//Output: 1389537
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace N_th_Tribonacci_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            Console.WriteLine(solution.solve_n_th_tribonacci_number(4)); 
+            Console.WriteLine(solution.solve_n_th_tribonacci_number(25));
+        }
+    }
+}
---
+++ C_Sharp/N-th Tribonacci Number/N-th Tribonacci Number/Solution.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N_th_Tribonacci_Number
+{
+    internal class Solution
+    {
+        public int solve_n_th_tribonacci_number(int n)
+        {
+            if(n == 0) return 0;
+            else if(n == 1 || n == 2) return 1;
+
+            int[] dp = new int[n + 1];
+            dp[0] = 0;
+            dp[1] = 1;
+            dp[2] = 1;
+
+            for(int i = 3; i <= n; i++)
+            {
+                dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+            }
+            return dp[n];
+        }
+    }
+}
