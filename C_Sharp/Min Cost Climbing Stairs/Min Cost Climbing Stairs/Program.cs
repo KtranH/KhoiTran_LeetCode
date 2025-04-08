9b01cba5fb7dad149696063483078d97b65bf558
KtranH <87740741+KtranH@users.noreply.github.com>
2025-04-08 10:41:17 +0700

    Solved Min Cost Climbing Stairs

---
+++ C_Sharp/Min Cost Climbing Stairs/Min Cost Climbing Stairs/Program.cs
@@ -0,0 +1,40 @@
+﻿//Example 1:
+
+//Input: cost = [10, 15, 20]
+//Output: 15
+//Explanation: You will start at index 1.
+//- Pay 15 and climb two steps to reach the top.
+//The total cost is 15.
+//Example 2:
+
+//Input: cost = [1, 100, 1, 1, 1, 100, 1, 1, 100, 1]
+//Output: 6
+//Explanation: You will start at index 0.
+//- Pay 1 and climb two steps to reach index 2.
+//- Pay 1 and climb two steps to reach index 4.
+//- Pay 1 and climb two steps to reach index 6.
+//- Pay 1 and climb one step to reach index 7.
+//- Pay 1 and climb two steps to reach index 9.
+//- Pay 1 and climb one step to reach the top.
+//The total cost is 6.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Min_Cost_Climbing_Stairs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] cost = { 10, 15, 20 };
+            Solution solution = new Solution();
+            int result = solution.solve_min_cost_climbing_stairs(cost);
+            Console.WriteLine(result); 
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C_Sharp/Min Cost Climbing Stairs/Min Cost Climbing Stairs/Solution.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Min_Cost_Climbing_Stairs
+{
+    internal class Solution
+    {
+        public int solve_min_cost_climbing_stairs(int[] cost)
+        {
+            int n = cost.Length;
+            int[] dp = new int[n + 1];
+            dp[0] = 0;
+            dp[1] = 0;
+
+            for (int i = 2; i <= n; i++)
+            {
+                dp[i] = Math.Min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
+            }
+
+            return dp[n];        
+        }
+    }
+}
