41f003992b470081c7e1e192689997d3983bf3aa
KtranH <87740741+KtranH@users.noreply.github.com>
2025-04-30 09:57:15 +0700

    Solved Climbing Stairs

---
+++ C_Sharp/Climbing Stairs/Climbing Stairs/Solution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Climbing_Stairs
+{
+    internal class Solution
+    {
+        public int solve_climbing_stairs(int n)
+        {
+            if(n == 1) return 1;
+            else if(n == 2) return 2;
+            int[] dp = new int[n + 1];
+            dp[0] = 1;
+            dp[1] = 1;
+            for (int i = 2; i <= n; i++)
+            {
+                dp[i] = dp[i - 1] + dp[i - 2];
+            }
+            return dp[n];
+        }
+    }
+}
---
+++ JavaScript/Climbing Stairs/app.js
@@ -0,0 +1,32 @@
+const n = 2;
+
+/*Example 1:
+
+Input: n = 2
+Output: 2
+Explanation: There are two ways to climb to the top.
+1. 1 step + 1 step
+2. 2 steps
+Example 2:
+
+Input: n = 3
+Output: 3
+Explanation: There are three ways to climb to the top.
+1. 1 step + 1 step + 1 step
+2. 1 step + 2 steps
+3. 2 steps + 1 step*/
+
+function solve_climbing_stairs(n) {
+    if (n === 1) return 1;
+    if (n === 2) return 2;
+    
+    let dp = new Array(n + 1);
+    dp[1] = 1;
+    dp[2] = 2;
+    for (let i = 3; i <= n; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+    return dp[n];
+}
+
+console.log(solve_climbing_stairs(n));
