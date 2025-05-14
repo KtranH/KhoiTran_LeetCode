ceaa64719a97bce5e782be305718c1ecc1535c69
KtranH <87740741+KtranH@users.noreply.github.com>
2025-05-14 10:43:07 +0700

    Solved Merge Sorted Array

---
+++ C_Sharp/Merge Sorted Array/Merge Sorted Array/Solution.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Merge_Sorted_Array
+{
+    internal class Solution
+    {
+        public void solve_merge_sorted_array(int[] nums1, int m, int[] nums2, int n) {
+            int i = m - 1;
+            int j = n - 1;
+            int k = m + n - 1;
+            while (j >= 0) {
+                if(i >= 0 && nums1[i] > nums2[j])
+                {
+                    nums1[k] = nums1[i];
+                    i--;
+                } 
+                else
+                {
+                    nums1[k] = nums2[j];
+                    j--;
+                }
+                k--;
+            }
+        }
+    }
+}
