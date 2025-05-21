e202a1158bac1ef2d8bb1a82187ca027d7fb5c42
KtranH <87740741+KtranH@users.noreply.github.com>
2025-05-21 14:25:19 +0700

    Solved Rotate Array

---
+++ C_Sharp/Rotate Array/Rotate Array/Solution.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rotate_Array
+{
+    internal class Solution
+    {
+        public void solve_rotate_array(int[] nums, int k)
+        {
+            int right = k % nums.Length;
+            int n = nums.Length;
+            int[] temp = (int[])nums.Clone();
+            for (int i = 0; i < n; i++)
+            {
+                nums[i] = temp[(i - right + n) % n];
+            }
+        }
+    }
+}
